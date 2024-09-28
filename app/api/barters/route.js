6445a3b694a7b79886900b899be8cb60ce3f13aa
Pavel <u6430176@au.edu>
2024-09-28 16:54:32 +0700

    Wrong status

--- app/api/barters/route.js
+++ app/api/barters/route.js
@@ -12,7 +12,8 @@ export async function GET() {
                     userId: {
                         not: session.user.id
                     }
-                }
+                },
+                status: "OPEN"
             },
             include: {
                 barterOwner: true,
